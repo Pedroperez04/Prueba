b79f33403255ff769709be93a91e3256a7800359
Pedroperez04 <pedroaperez021@gmail.com>
2023-04-17 17:21:50 -0300

    agregando codigo al nuevo script
    Agregando codigo al script movement para lograr que el personaje se mueva al añadirselo

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    [SerializeField] private float playerSpeed = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,14 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        var m_Horizontal = Input.GetAxis("Horizontal");
+        var m_Vertical = Input.GetAxis("Vertical");
+        var m_direction = new Vector3(m_Horizontal, 0, m_Vertical);
+
+        Move(m_direction);
+    }
+    private void Move(Vector3 mov_direction)
+    {
+        transform.position += mov_direction * (playerSpeed * Time.deltaTime);
     }
 }
